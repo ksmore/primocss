61858b2be0a9c6ff8048a9c1755ea7055f26b933
Richard McCartney <richard@mccartney.io>
2017-03-06 00:35:31 +0000

    updating version

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ cssnano = require('gulp-cssnano'),
 sassLint = require('gulp-sass-lint'),
 rename = require('gulp-rename'),
 sourcemaps = require('gulp-sourcemaps'),
-sassPath = 'scss/primocss-0.0.6.scss';
+sassPath = 'scss/primocss-0.0.7.scss';
 
 // Gulp help instructions triggered as Gulp default task
 gulp.task('help', function() {
