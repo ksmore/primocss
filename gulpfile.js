159d516f50406ec339684077899ff2680efe307c
Richard McCartney <richard@mccartney.io>
2017-03-23 17:46:30 +0000

    Updating grid

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ cssnano = require('gulp-cssnano'),
 sassLint = require('gulp-sass-lint'),
 rename = require('gulp-rename'),
 sourcemaps = require('gulp-sourcemaps'),
-sassPath = 'scss/primocss-1.0.0.scss';
+sassPath = 'scss/primocss.scss';
 
 // Gulp help instructions triggered as Gulp default task
 gulp.task('help', function() {
